0c77f54a3ba2a7d32155a2afe962f20a7f53f4c3
BorisSonin <bsonin85@gmail.com>
2023-01-20 17:25:22 +0300

    Initial Commit

---
+++ SliderSwiftUI+UIKit/SliderSwiftUI_UIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderSwiftUI_UIKitApp.swift
+//  SliderSwiftUI+UIKit
+//
+//  Created by Борис Сонин on 20.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SliderSwiftUI_UIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
